3d9e1a528592f190051ef43abaa976a1eecff126
Laraib khan <114330724+Laraibkhan13@users.noreply.github.com>
2024-09-17 21:36:34 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Auth from './components/Auth'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1>Firebase</h1>
+        <Auth></Auth>
+      </div>
+    </>
+  )
+}
+
+export default App
